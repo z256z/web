328f61b3e3c47466a1a77f305120620569e6e898
z256 <edwardelric32578@gmail.com>
2025-09-02 04:21:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: '8080'});
+
+console.log("Listening on port 8080");
+
+server.on('connection', socket => {
+
+    socket.on('message', message => {
+
+        socket.send('Connected ${message}');
+    })
+})
+
 console.log("Hello");
